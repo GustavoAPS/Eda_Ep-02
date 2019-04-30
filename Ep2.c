780cd0c7f2abd0d8a6a6514fd2b170745c3292cb
Gustavo Afonso <gugustavo137@gmail.com>
2019-04-30 15:28:01 -0300

    error fix when reading image to matrix

--- Ep2.c
+++ Ep2.c
@@ -1,8 +1,9 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
-int main(){
-
+int ilbp(int a ,int b)
+{
 FILE *fp; // ponteiro para o arquivo
 
 char file_name[] = "grass_01.txt"; //caminho do arquivo
@@ -10,6 +11,7 @@ char file_name[] = "grass_01.txt"; //caminho do arquivo
 fp = fopen(file_name,"r");
 
 int matrix[1025][1025];//linhas/colunas
+char line[2125];
 int l = 0;
 int c = 0;
 char ch;
@@ -17,36 +19,87 @@ char pixel_concatenation[4];
 int aux = 0; 
 int number=0;
 
+        if ( fp != NULL )
+        {
+        	while ( fgets ( line, sizeof line, fp ) != NULL ) /* read a line */
+        	{	
+			for (int i = 0 ; i < strlen(line) ; i++)
+			{
+				if(line[i] == ';' || line[i] < 48 || line[i] > 57 )
+        			{
+								
+                			pixel_concatenation[aux] = '\0';//garante o funcionamento de atoi
+                			number = atoi(pixel_concatenation);
+					matrix[l][c]=number;
+
+                			if (c == 1024) /// ATENTION
+                			{
+                        			c = 0;
+                        			l++;
+                			}else{
+	                		c++;
+					}
+
+                			pixel_concatenation[0] = '.';
+                			pixel_concatenation[1] = '.';
+                			pixel_concatenation[2] = '.';
+                			aux = 0;
+        			}
+
+        			else
+        			{
+        			pixel_concatenation[aux] = line[i];
+        			aux++;
+        			}
+
+			}
+	//			printf("%c", line[i]);
+
+			//printf("\n");
+			//printf("%s \n", line);
+        		//fputs ( line, stdout ); /* write the line */
+        	}
+        }
+/*
 while ( (ch = fgetc(fp))  != EOF ){
-	if(ch == ';' )
+
+	if(ch == ';')
 	{
-	pixel_concatenation[aux] = '\0';
-	number = atoi(pixel_concatenation);
-	if (c == 1024){
-	c = 0;
-	l++;
+		pixel_concatenation[aux] = '\0';
+		number = atoi(pixel_concatenation);
+
+		if (c == 1024)
+		{
+			c = 0;
+			l++;
+		}
+
+		matrix[l][c]=number;
+		c++;
+		pixel_concatenation[0] = '.';
+		pixel_concatenation[1] = '.';
+		pixel_concatenation[2] = '.';
+		aux = 0;
 	}
-	matrix[l][c]=number;
-	c++;
-	//printf("%d\n",number);
-	pixel_concatenation[0] = '.';
-	pixel_concatenation[1] = '.';
-	pixel_concatenation[2] = '.';
-	aux = 0;
-	}
-	else{
+	else
+	{
 	pixel_concatenation[aux] = ch; 
 	aux++;
 	}
-}
+}*/
 fclose(fp);
 
-for (int a = 0; a < 1025; a++){
-	for (int b = 0; b < 1025; b++){
-	printf("%d ", matrix[a][b]);
-	}
-printf ("\n");
+return matrix[a][b];
 }
+
+int main ()
+{
+int a,b;
+printf("entre com a linha: ");
+scanf("%d", &a);
+printf("entre com a coluna: ");
+scanf("%d", &b);
+printf("Numero na posição : %d \n", ilbp(a,b));
 
 return 0;
 }
